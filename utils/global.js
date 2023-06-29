8ea21c63700da3dac0774f6204e40abdd062fde2
wei <liaoqifen@sina.com>
2023-06-29 18:23:43 +0800

    更新URL

--- utils/global.js
+++ utils/global.js
@@ -5,7 +5,7 @@ export default {
 	},
 	//REQUEST_URL: 'http://8.214.55.8'
 	REQUEST_URL: 'http://39.108.116.68:8009',
-	//REQUEST_URL: 'http://192.168.0.15:8083'
 	WS_URL: 'ws://39.108.116.68:8009/ws'
-	
+	//REQUEST_URL: 'http://192.168.0.15:8083',
+	//WS_URL: 'ws://192.168.0.15:9326/ws'
 }
